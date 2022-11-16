541f4462aa7cfac9c600d462c47f4fcae00a7d3f
Mireya <thewinlow1@gmail.com>
2022-11-16 12:31:23 +0100

    merge realizado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Estudiante " + args[0] + ":" + Proyectofuturo.veredicto("5", "4"));
-        /**Mireya Martínez Pérez*/
-    }
+
 }
--- src/Proyectofuturo.java
+++ src/Proyectofuturo.java
@@ -1,5 +1,8 @@
 public class Proyectofuturo {
-
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + ":" + veredicto("5", "4"));
+        /**Mireya Martínez Pérez*/
+    }
 
     private static String veredicto(String ciclos, String horas){
         if (ciclos.equals(horas))
