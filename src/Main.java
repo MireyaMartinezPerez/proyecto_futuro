22caa129b7c17d5f7eda25fdf7daaa4098c65152
Mireya <thewinlow1@gmail.com>
2022-11-16 12:15:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + ":" + Proyectofuturo.veredicto("5", "4"));
+    }
+}
---
+++ src/Proyectofuturo.java
@@ -0,0 +1,13 @@
+public class Proyectofuturo {
+
+
+    static String veredicto(String ciclos, String horas){
+        if (ciclos.equals(horas))
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        if (Integer.valueOf(horas)<Integer.valueOf(ciclos)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+
+}
